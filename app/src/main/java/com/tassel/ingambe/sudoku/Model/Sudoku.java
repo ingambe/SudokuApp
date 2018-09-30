3fcbe87e40376958a5f1bd680c88b8768d833e14
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-09-30 11:32:52 +0200

    Initial commit

---
+++ app/src/main/java/com/tassel/ingambe/sudoku/Model/Sudoku.java
@@ -0,0 +1,32 @@
+package com.tassel.ingambe.sudoku.Model;
+
+public class Sudoku {
+
+    int size;
+    private int fullGrid[][];
+    private boolean holeGrid[][];
+
+    public Sudoku(int size, int[][] fullGrid, boolean[][] holeGrid) {
+        this.size = size;
+        this.fullGrid = fullGrid;
+        this.holeGrid = holeGrid;
+    }
+
+    public Sudoku(int size) {
+        this.size = size;
+        this.fullGrid = new int[size][size];
+        this.holeGrid = new boolean[size][size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[][] getFullGrid() {
+        return fullGrid;
+    }
+
+    public boolean[][] getHoleGrid() {
+        return holeGrid;
+    }
+}
